25efb1887e0ebcec7ec156fcdd1e9b5812579f11
shrushtikaza <130393506+shrushtikaza@users.noreply.github.com>
2025-07-15 10:50:14 +0530

    adjusted placement of app on screen

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, screen } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
 function createWindow() {
+  const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
+
+  const winWidth = 240;
+  const winHeight = 328;
+
   const win = new BrowserWindow({
-    width: 240,
-    height: 328,
+    width: winWidth,
+    height: winHeight,
+    x: screenWidth - winWidth - 20,
+    y: 50,                           
+    frame: false,
+    transparent: false,
+    resizable: false,
+    hasShadow: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
